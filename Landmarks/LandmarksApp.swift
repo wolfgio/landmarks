53d176f41fd531dc76be7c72e74eb76b2a096524
Giovanny Piovesan <giowrp@gmail.com>
2021-07-04 20:43:34 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Giovanny Piovesan on 04/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
